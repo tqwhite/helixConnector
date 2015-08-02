e1f42aafc8e577647c6bc799832cb8e5fd7c0158
TQ White II <tq@justkidding.com>
2015-08-02 12:02:18 -0500

    revised to use Tape for testing instead of Mocha, a little file cleanup, too

--- test/formatFunctions.js
+++ test/formatFunctions.js
@@ -1,5 +1,4 @@
-
-var assert = require("assert")
+var test = require('tape');
 
 var helixConnector = require('../helixConnector.js');
 var config = require('../../config/qbook.js');
@@ -9,35 +8,20 @@ helixConnector = new helixConnector({
 	helixAccessParms: config.getHelixParms()
 });
 
-//new Date(year, month[, day[, hour[, minutes[, seconds[, milliseconds]]]]]);		
+test('connector formatFunctions', function(t) {
+	var testDescription;
+	t.plan();
 
-describe('Format functions', function() {
-	describe('Helix Date', function() {
-		var testDate = new Date(
+	var testDate = new Date('2015', '5', '29', '8', '38', '39'); //I don't understand why I have to type '5' to get June in the date(), however, when I don't specify the date, as in new Date(), it works correctly.
+	testDescription="conversion to Helix date"
+	t.equal('06/29/15 08:38:39 AM', helixConnector.formatFunctions.helixDateTime(testDate), testDescription);
+	
+	testDescription="date stamp Helix format";
+	t.ok(helixConnector.formatFunctions.helixDateTimeNow().match(/^\d\d\/\d\d\/\d\d \d\d:\d\d:\d\d (A|P)M$/), testDescription);
 
-		'2015',
-		'5', //I don't understand why I have to type '5' to get June in the date()
-		'29', //however, when I don't specify the date, as in new Date(), it works correctly.
-		'8',
-		'38',
-		'39'
+	testDescription="refId length";
+	t.equal(36, helixConnector.formatFunctions.refId().length, testDescription);
 
-		);
-
-		it('should be formatted 6/29/15 08:38:39 AM', function() {
-			assert.equal('06/29/15 08:38:39 AM', helixConnector.formatFunctions.helixDateTime(testDate));
-		});
-
-		describe('RefId', function() {
-			it('should be 36 characters long', function() {
-				assert.equal(36, helixConnector.formatFunctions.refId().length);
-			});
-		});
-
-		describe('Helix Date Stamp', function() {
-			it('should match mm/dd/yy hh:mm:ss (A|P)M', function() {
-				assert.ok(helixConnector.formatFunctions.helixDateTimeNow().match(/^\d\d\/\d\d\/\d\d \d\d:\d\d:\d\d (A|P)M$/));
-			});
-		});
-	});
+	t.end();
 });
+
