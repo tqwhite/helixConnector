a6f66e2b4a5f361176f3131e029905fa676646d3
TQ White II <tq@justkidding.com>
2021-02-15 17:07:33 -0600

    added support for directory for automatically included schmema files

--- helixAjax/lib/schema-map-assembler/schema-map-assembler.js
+++ helixAjax/lib/schema-map-assembler/schema-map-assembler.js
@@ -28,6 +28,31 @@ var moduleFunction = function(args) {
 
 	//METHODS AND PROPERTIES ====================================
 
+	const getAutoIncludeDirectoryItems = autoIncludeDirectoryPath => {
+		const autoIncludeItems = qtools.fs.readdirSync(autoIncludeDirectoryPath);
+		let outObj = {};
+		let count = 0;
+		autoIncludeItems
+			.filter(fileName => fileName.match(/\.json$/))
+			.forEach(fileName => {
+				const itemPath = path.join(autoIncludeDirectoryPath, fileName);
+				try {
+					const contents = qtools.fs.readFileSync(itemPath).toString();
+					const element = JSON.parse(contents);
+
+					outObj = { ...outObj, ...element };
+
+					count++;
+				} catch (e) {
+					console.log(`BAD JSON ${itemPath}`);
+				}
+			});
+
+		console.log(`added ${count} items from ${autoIncludeDirectoryPath}`);
+
+		return outObj;
+	};
+
 	const getSchemaMap = schemaMapPath => {
 		if (!qtools.realPath(schemaMapPath)) {
 			const message =
@@ -36,7 +61,7 @@ var moduleFunction = function(args) {
 			return message;
 		}
 		let schemaMap;
-		
+
 		const schemaMapJson = qtools.fs.readFileSync(schemaMapPath);
 
 		try {
@@ -44,11 +69,9 @@ var moduleFunction = function(args) {
 		} catch (e) {
 			console.log('schemaMap main file failed to parse: ' + schemaMapPath);
 			throw 'schemaMap main file failed to parse';
-		} 
-		 if (schemaMap.includes && schemaMap.includes.length) {
-			 
+		}
+		if (schemaMap.includes && schemaMap.includes.length) {
 			schemaMap.includes.reduce((result, item) => {
-				 
 				const includePath = path.join(schemaMapPath, '../', item);
 				const includeJson = qtools.fs.readFileSync(includePath);
 
@@ -66,8 +89,25 @@ var moduleFunction = function(args) {
 
 				return result;
 			}, schemaMap);
-		} 
-		 return schemaMap;
+
+			if (typeof schemaMap.autoIncludeDirectoryPath == 'string') {
+				const autoIncludeDirectoryPath = path.join(
+					schemaMapPath,
+					'../',
+					schemaMap.autoIncludeDirectoryPath
+				);
+				const autoIncludeSchemaItems = getAutoIncludeDirectoryItems(
+					autoIncludeDirectoryPath
+				);
+
+				schemaMap.schemaMap = {
+					...schemaMap.schemaMap,
+					...autoIncludeSchemaItems
+				};
+			}
+		}
+
+		return schemaMap;
 	};
 
 	//API ENDPOINTS ====================================
