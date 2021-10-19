76aabe21d19f0159f36b58a6fd92588bc4cb9e12
TQ White II <tq@justkidding.com>
2021-10-19 10:46:31 -0500

    important fixes, production release

--- helixAjax/lib/version.js
+++ helixAjax/lib/version.js
@@ -1,7 +1,9 @@
-module.exports=`hxc090121.3 DEV`
+module.exports=`hxc090121.3`
 
+//hxc090121.3
 //updated summarize-configs.js to recognize remote control scripts as valid. 9/23/21
 //removed cruft from fileOne.json
 //changed express-rate-limit values to 60/minute
 //fixed major bug in helix-data where index to lookup data type conversions was always using StringType
+//fixed problem with testHelixAlive. it now works with Helix RADE
 
